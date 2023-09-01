ff35fe7e565020afe32a5ff1902606c29bd09e2b
KosmyninV <kos.vad2@gmail.com>
2023-09-01 14:28:16 +0700

    task-7-resizeFix3

--- src/components/Layout/AddNote/ImagesList/ImagesItem/ImagesItem.jsx
+++ src/components/Layout/AddNote/ImagesList/ImagesItem/ImagesItem.jsx
@@ -1,15 +1,21 @@
 import React, {useEffect, useRef} from 'react';
 import styles from './ImagesItem.module.css';
+import {debounce} from "../../../../../utils/debounce";
 
 function ImagesItem({note, altText}) {
 	const hiddenImg = useRef(null)
-	useEffect(()=>{
+	const resizeImg = () => {
 		if (hiddenImg.current){
 			if (hiddenImg.current.offsetWidth / hiddenImg.current.offsetHeight < 1){
 				hiddenImg.current.parentNode.style.gridRow = 'span 2'
 			}
 		}
-	},[])
+	}
+	useEffect(() => {
+		resizeImg()
+		window.addEventListener('resize', debounce(resizeImg))
+		return ()=>{window.addEventListener('resize', debounce(resizeImg))}
+	}, [])
 
 
 	return (
